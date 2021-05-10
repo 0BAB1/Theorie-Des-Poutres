b788fd675701da4f2dfa0aa4a84d438d5887bf06
0BAB1 <hb.hugobabin@gmail.com>
2021-05-10 16:48:53 +0200

    last changes aborting project

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ class Application(tk.Tk):
         self.geometry("720x480")
         self.make_widgets()
         self.config(menu=self.menuBar, bg="skyblue")
+        self.resizable(False, False)
 
     def make_widgets(self):
         """Create All Widget On The Front Page"""
@@ -23,9 +24,9 @@ class Application(tk.Tk):
         flexionMenu.add_command(label="force en bout", command = lambda : self.activate_flexion(0))
         flexionMenu.add_command(label="force repartie", command = lambda : self.activate_flexion(1))
         flexionMenu.add_separator()
-        flexionMenu.add_command(label="moment, double appui")
-        flexionMenu.add_command(label="force x=a")
-        flexionMenu.add_command(label="force x=L/2")
+        flexionMenu.add_command(label="moment, double appui", command = lambda : self.activate_flexion(2))
+        flexionMenu.add_command(label="force x=a", command = lambda : self.activate_flexion(3))
+        flexionMenu.add_command(label="force x=L/2", command = lambda : self.activate_flexion(4))
         self.menuBar.add_cascade(label="flexion", menu=flexionMenu)
 
         #menus to select which section
@@ -37,15 +38,24 @@ class Application(tk.Tk):
         self.menuBar.add_cascade(label="section", menu=sectionMenu)
 
         #create the left frame
-        self.left_frame = tk.Frame(self, width=240, height=480)
-        self.left_frame.grid(row=0, column=0, padx=10, pady=5, sticky="NESW")
+        self.left_frame = tk.Frame(self)
+        self.left_frame.grid(row=0, column=0)
 
-        #create differetn interfaces for each flexion cases, and grid it into a frame
+        #create differetn interfaces for each flexion cases, and add it to our frames array, ready to grid
         frame1 = Frame1(self.left_frame)
         self.frames.append(frame1)
         
         frame2 = Frame2(self.left_frame)
         self.frames.append(frame2)
+
+        frame21 = Frame21(self.left_frame)
+        self.frames.append(frame21)
+
+        frame22 = Frame22(self.left_frame)
+        self.frames.append(frame22)
+
+        frame23 = Frame23(self.left_frame)
+        self.frames.append(frame23)
 
     def activate_flexion(self, i):
         for frame in self.frames:
--- frames.py
+++ frames.py
@@ -7,15 +7,21 @@ class Frame1(tk.Frame):
         #call the tk.Frame init, markin the master, specified when class called in app.py
         super().__init__(master, width=220, height=460)
 
+        #result
+        self.result_label = tk.Label(self)
+
         #igz
-        self.igz_label = tk.Label(self, text="igz")
+        self.igz_label = tk.Label(self, text="igz (en mm^4) : ")
         self.igz = tk.Entry(self)
         #Lenght
-        self.l_label = tk.Label(self, text="L")
+        self.l_label = tk.Label(self, text="L (en mm)")
         self.l = tk.Entry(self)
         #actual force
-        self.f_label = tk.Label(self, text="F")
+        self.f_label = tk.Label(self, text="F (en Newtons)")
         self.f = tk.Entry(self)
+
+        #button
+        self.enter = tk.Button(self, text="valider", height=1, width=20, command=self.proceed)
 
         #grid igz
         self.igz_label.grid(row=0,column=0)
@@ -29,8 +35,7 @@ class Frame1(tk.Frame):
         self.f_label.grid(row=2,column=0)
         self.f.grid(row=2,column=1)
 
-        #button and grid
-        self.enter = tk.Button(self, text="valider", height=1, width=20, command=self.proceed)
+        #grid button
         self.enter.grid(row=3)
 
         #image and grid
@@ -39,16 +44,185 @@ class Frame1(tk.Frame):
         self.img.grid(row=4)
     
     def proceed(self):
-        print("calcul")
+        f = int(self.f.get())
+        l = int(self.l.get())
+        i = int(self.igz.get())
+        e = 210000
+        yMax = (f*l^3)/(3*e*i)
+
+        self.result_label['text'] = yMax
+        self.result_label.grid()
 
 class Frame2(tk.Frame):
     """this frame is force evenly placed force"""
     def __init__(self, master):
-        #igz
+        #call the tk.Frame init, markin the master, specified when class called in app.py
         super().__init__(master, width=220, height=460)
-        self.igz_label = tk.Label(self, text="igz2 omg ca a changé")
+
+        #result
+        self.result_label = tk.Label(self)
+
+        #igz
+        self.igz_label = tk.Label(self, text="igz (en mm^4) : ")
         self.igz = tk.Entry(self)
+        #Lenght
+        self.l_label = tk.Label(self, text="L (en mm)")
+        self.l = tk.Entry(self)
+        #actual force
+        self.w_label = tk.Label(self, text="W (en N/mm)")
+        self.w = tk.Entry(self)
+
+        #button
+        self.enter = tk.Button(self, text="valider", height=1, width=20)
 
         #grid igz
         self.igz_label.grid(row=0,column=0)
         self.igz.grid(row=0,column=1)
+
+        #grid L
+        self.l_label.grid(row=1,column=0)
+        self.l.grid(row=1,column=1)
+
+        #grid force
+        self.w_label.grid(row=2,column=0)
+        self.w.grid(row=2,column=1)
+
+        #grid button
+        self.enter.grid(row=3)
+
+        #image and grid
+        self.file = tk.PhotoImage(file="assets/2.png")
+        self.img = tk.Label(self, image=self.file)
+        self.img.grid(row=4)
+
+class Frame21(tk.Frame):
+    """this frame is force evenly placed force"""
+    def __init__(self, master):
+        #call the tk.Frame init, markin the master, specified when class called in app.py
+        super().__init__(master, width=220, height=460)
+
+        #result
+        self.result_label = tk.Label(self)
+
+        #igz
+        self.igz_label = tk.Label(self, text="igz (en mm^4) : ")
+        self.igz = tk.Entry(self)
+        #Lenght
+        self.l_label = tk.Label(self, text="L (en mm)")
+        self.l = tk.Entry(self)
+        #actual force
+        self.mo_label = tk.Label(self, text="Moment (en N.mm)")
+        self.mo = tk.Entry(self)
+
+        #button
+        self.enter = tk.Button(self, text="valider", height=1, width=20)
+
+        #grid igz
+        self.igz_label.grid(row=0,column=0)
+        self.igz.grid(row=0,column=1)
+
+        #grid L
+        self.l_label.grid(row=1,column=0)
+        self.l.grid(row=1,column=1)
+
+        #grid force
+        self.mo_label.grid(row=2,column=0)
+        self.mo.grid(row=2,column=1)
+
+        #grid button
+        self.enter.grid(row=3)
+
+        #image and grid
+        self.file = tk.PhotoImage(file="assets/21.png")
+        self.img = tk.Label(self, image=self.file)
+        self.img.grid(row=4)
+
+class Frame22(tk.Frame):
+    """this frame is force evenly placed force"""
+    def __init__(self, master):
+        #call the tk.Frame init, markin the master, specified when class called in app.py
+        super().__init__(master, width=220, height=460)
+
+        #result
+        self.result_label = tk.Label(self)
+
+        #igz
+        self.igz_label = tk.Label(self, text="igz (en mm^4) : ")
+        self.igz = tk.Entry(self)
+        #Lenght
+        self.l_label = tk.Label(self, text="L (en mm)")
+        self.l = tk.Entry(self)
+        #a
+        self.a_label = tk.Label(self, text="a (en mm")
+        self.a = tk.Entry(self)
+        #p
+        self.p_label = tk.Label(self, text="P (en Newtons)")
+        self.p = tk.Entry(self)
+        #button
+        self.enter = tk.Button(self, text="valider", height=1, width=20)
+
+        #grid igz
+        self.igz_label.grid(row=0,column=0)
+        self.igz.grid(row=0,column=1)
+
+        #grid L
+        self.l_label.grid(row=1,column=0)
+        self.l.grid(row=1,column=1)
+
+        #grid a
+        self.a_label.grid(row=2,column=0)
+        self.a.grid(row=2,column=1)
+
+        #grid p
+        self.p_label.grid(row=3, column=0)
+        self.p.grid(row=3, column=1)
+
+        #grid button
+        self.enter.grid(row=4)
+
+        #image and grid
+        self.file = tk.PhotoImage(file="assets/22.png")
+        self.img = tk.Label(self, image=self.file)
+        self.img.grid(row=5)
+
+class Frame23(tk.Frame):
+    """this frame is force evenly placed force"""
+    def __init__(self, master):
+        #call the tk.Frame init, markin the master, specified when class called in app.py
+        super().__init__(master, width=220, height=460)
+
+        #result
+        self.result_label = tk.Label(self)
+
+        #igz
+        self.igz_label = tk.Label(self, text="igz (en mm^4) : ")
+        self.igz = tk.Entry(self)
+        #Lenght
+        self.l_label = tk.Label(self, text="L (en mm)")
+        self.l = tk.Entry(self)
+        #actual force
+        self.p_label = tk.Label(self, text="P (en Newtons)")
+        self.p = tk.Entry(self)
+
+        #button
+        self.enter = tk.Button(self, text="valider", height=1, width=20)
+
+        #grid igz
+        self.igz_label.grid(row=0,column=0)
+        self.igz.grid(row=0,column=1)
+
+        #grid L
+        self.l_label.grid(row=1,column=0)
+        self.l.grid(row=1,column=1)
+
+        #grid force
+        self.p_label.grid(row=2,column=0)
+        self.p.grid(row=2,column=1)
+
+        #grid button
+        self.enter.grid(row=3)
+
+        #image and grid
+        self.file = tk.PhotoImage(file="assets/23.png")
+        self.img = tk.Label(self, image=self.file)
+        self.img.grid(row=4)
